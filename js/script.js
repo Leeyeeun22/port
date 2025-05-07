91a6e8a48b4ee192abcc1d64c30b9762542da104
Leeyeeun <com0748@naver.com>
2025-05-07 22:16:36 +0900

    commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+new fullpage('#full-page',{
+    // licenseKey: '',
+    // sectionsColor: ['#ff0000','#00ff00','#0000ff','#aaaaaa'],
+    navigation: true,
+    responsiveWidth: 1023, // 너비가 1023일때 수동 원페이지 -> 스크롤
+    // scrollingSpeed: 500,
+    // scrollBar: true,
+    navigationTooltips:['home','skills','portfolio','about'],
+});
+
